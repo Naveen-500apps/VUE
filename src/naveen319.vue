afa339f7772f73aa7ea84fce09ca6195005fe3ad
Naveen-500apps <naveen.addakula@500apps.com>
2022-03-23 19:29:48 +0530

    hello

---
+++ src/naveen319.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+                  NaVeEn </div>
+<h2>message:{{message}}
+</h2>
+    <h2>name:{{name}}
+    </h2>
+    <h2>number:{{number}}
+    </h2>
+    <h2>workingcompany:{{workingcompany}}
+    </h2>
+</template>
+<script>
+
+export default {
+name:"NaVeen",
+props:{
+
+message : String,
+name : String,
+number : Number,
+workingcompany: String
+}
+}
+</script>
